fb86331da84408e3a364d8ae2914c4013dc91632
Benoit Travers <benoittravers@Benoits-MacBook-Pro.local>
2023-05-15 11:42:19 +0800

    Adding files

--- JS101-JS119/easy1-5.js
+++ JS101-JS119/easy1-5.js
@@ -3,23 +3,14 @@ let readlineSync = require("readline-sync");
 function billCalculator() {
   console.log('What is the bill? ');
   let billAmount = readlineSync.prompt();
-  
-  console.log('What is the tip percentage? ')
-  let billPercentage = parseInt(readlineSync.prompt() * 0.01);
-  let tipAmount = parseInt(billAmount * billPercentage);
-  let totalAmount = (tipAmount + billAmount);
-  
+
+  console.log('What is the tip percentage? ');
+  let billPercentage = readlineSync.prompt() * 0.01;
+  let tipAmount = billAmount * billPercentage;
+  let totalAmount = +tipAmount + +billAmount;
+
   console.log(`\n \n The tip is $${tipAmount}`);
   console.log(`The total is $${totalAmount}`);
 }
 
 billCalculator();
-
-
-/*
-What is the bill? 200
-What is the tip percentage? 15
-
-The tip is $30.00
-The total is $230.00
-*/
